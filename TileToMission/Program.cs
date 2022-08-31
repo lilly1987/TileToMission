1986729423ed5662a0e5ad818d77c4fcfd2e81a6
lilly <lilly1987@naver.com>
2022-08-31 20:46:59 +0900

    ...

--- TileToMission/Program.cs
+++ TileToMission/Program.cs
@@ -5,13 +5,15 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static string[] tiles;
-        //static List<string> tiles2=new List<string>();
+        //static string[] tiles;
+        static IEnumerable<string?> dirs;
         static string[] missions;
+        static Dictionary<string, StringBuilder> dird = new Dictionary<string, StringBuilder>();
+        static Dictionary<string, string> txtd = new Dictionary<string, string>();
 
         static void Main(string[] args)
         {
-            if (args.Length==0)
+            if (args.Length == 0)
             {
                 Console.WriteLine("need folder");
             }
@@ -21,32 +23,49 @@ namespace MyApp // Note: actual namespace depends on the project name.
             {
                 // 디렉토리일 경우
                 //Console.WriteLine("folder");
-                tiles=Directory.GetFiles(args[0] + "\\biomes", "*.tile", SearchOption.AllDirectories);
-                missions=Directory.GetFiles(args[0]+ "\\missions", "*.mission.tmp", SearchOption.AllDirectories);
+                //tiles = Directory.GetFiles(args[0] + "\\biomes", "*.tile", SearchOption.AllDirectories);
+                dirs = Directory.GetDirectories(args[0] + "\\biomes").Select(x => Path.GetFileName(x));
+                missions = Directory.GetFiles(args[0] + "\\missions", "*.mission.tmp", SearchOption.AllDirectories);
             }
 
-            //tiles2= tiles.Select(s => s.Replace(args[0]+"\\", String.Empty).Replace('\\','/')).ToList();
-            StringBuilder sb = new StringBuilder();
-            foreach (var tile in tiles)
+            string tTxt;
+            dird["all"] = new StringBuilder();
+            foreach (var item in dirs)
             {
-                if (Regex.IsMatch(File.ReadAllText(tile), "is_hidden\\s+\"1\""))
+                Console.WriteLine(item);
+                dird[item] = new StringBuilder();
+                foreach (var tile in Directory.GetFiles(args[0] + "\\biomes\\"+ item, "*.tile", SearchOption.AllDirectories))
                 {
-                    continue;
+                    if (Regex.IsMatch(File.ReadAllText(tile), "is_hidden\\s+\"1\""))
+                    {
+                        continue;
+                    }
+                    //Console.WriteLine(tile);
+                    tTxt = tile.Replace(args[0] + "\\", String.Empty).Replace('\\', '/') + "\n";
+                    dird["all"].Append(tTxt);
+                    dird[item].Append(tTxt);
                 }
-                Console.WriteLine(tile);
-                //tiles2.Add(tile.Replace(args[0] + "\\", String.Empty).Replace('\\', '/'));
-                sb.Append(tile.Replace(args[0] + "\\", String.Empty).Replace('\\', '/')+"\n");
             }
-            var tileTxt= sb.ToString();
+
+            foreach (var item in dird.Keys)
+            {
+                txtd[item] = dird[item].ToString();
+            }
+
             string mTxt;
             foreach (var mission in missions)
             {
                 Console.WriteLine(mission);
                 mTxt = File.ReadAllText(mission);
-                File.WriteAllText(Path.ChangeExtension(mission,null), Regex.Replace(mTxt, "//#tile-all\r?\n", tileTxt));
+
+                foreach (var item in txtd.Keys)
+                {
+                    mTxt = Regex.Replace(mTxt, "//#tile-" + item + "\r?\n", txtd[item]);
+                }
+                File.WriteAllText(Path.ChangeExtension(mission, null), mTxt);
             }
 
-            Console.ReadLine();
+            //Console.ReadLine();
         }
 
     }
